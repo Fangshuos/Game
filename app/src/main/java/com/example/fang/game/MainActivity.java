598df1b9670f15b09401bb4d96c453ba87db9319
Fangshuos <13241218682@163.com>
2017-11-01 15:47:11 +0800

    My Commit2

--- app/src/main/java/com/example/fang/game/MainActivity.java
+++ app/src/main/java/com/example/fang/game/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.fang.game;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv=new TextView(this);
+        ImageView im=new ImageView(this);
     }
 }
